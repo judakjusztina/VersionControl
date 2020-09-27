72c4bf34549fdaea317cebb35f1d3573bd8fbd09
Jusztina Judák <jusztina.judak@stud.uni-corvinus.hu>
2020-09-27 17:39:09 +0200

    kettes

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -15,6 +15,16 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.LastName;
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
