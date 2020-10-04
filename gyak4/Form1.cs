614d25735b5d87acb9a678893a84d4e9e506e9d9
Jusztina Judák <jusztina.judak@stud.uni-corvinus.hu>
2020-10-04 18:15:47 +0200

    formazasok és get

--- gyak4/Form1.cs
+++ gyak4/Form1.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
 using System;
+using System.Drawing;
 
 namespace gyak4
 {
@@ -23,6 +24,7 @@ namespace gyak4
             InitializeComponent();
             LoadData();
             CreateExcel();
+
         }
         private void LoadData()
         {
@@ -54,10 +56,77 @@ namespace gyak4
                 xlApp = null;
             }
 
+
         }
+
         private void CreateTable()
         {
+            string[] headers = new string[] {
+             "Kód",
+             "Eladó",
+             "Oldal",
+             "Kerület",
+             "Lift",
+             "Szobák száma",
+             "Alapterület (m2)",
+             "Ár (mFt)",
+             "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < 8; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+                object[,] values = new object[Flats.Count, headers.Length];
+                int counter = 0;
+                foreach (Flat f in Flats)
+                {
+                    values[counter, 0] = f.Code;
+                    // ...
+                    values[counter, 8] = "";
+                    counter++;
+                    }
+
+                xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+                Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+                headerRange.Font.Bold = true;
+                headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+                headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                headerRange.EntireColumn.AutoFit();
+                headerRange.RowHeight = 40;
+                headerRange.Interior.Color = Color.LightBlue;
+                headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            }
+           
+            }
+
+        private string GetCell(int x, int y)
+        {
+
+
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
 
         }
     }
-}
+
+
+            }
+            
+
+        
+    
+
