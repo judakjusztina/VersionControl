8733d69b668407af2c85d44ee4402f1f50ad6d7e
Jusztina Judák <jusztina.judak@stud.uni-corvinus.hu>
2020-11-29 17:13:04 +0100

    játékosok

--- tiedikhet/Form1.cs
+++ tiedikhet/Form1.cs
@@ -15,15 +15,25 @@ namespace tiedikhet
     {
         GameController gc = new GameController();
         GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+            
+            for(int i = 1; i<populationSize;i++)
+            {
+                gc.AddPlayer();
+            }
 
-            //gc.AddPlayer();
-            //gc.Start(true);
+            gc.Start(true);
         }
 
     }
